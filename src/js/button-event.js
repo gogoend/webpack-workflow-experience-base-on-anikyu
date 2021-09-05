d663fee3ac73cdb4873315cbb73d76e3e3f58723
gogoend <gogoend@qq.com>
2021-09-06 00:07:29 +0800

    feat(rxjs): 将button点击事件由addEventListener改为fromEvent...subscribe...（rxjs）

--- src/js/button-event.js
+++ src/js/button-event.js
@@ -1,6 +1,8 @@
+import { fromEvent } from 'rxjs';
+
 const button = document.getElementById('console-btn');
 
 // 热更新不能够解绑事件
-button.addEventListener('click', () => {
-	console.log(222233334444);
+fromEvent(button, 'click').subscribe((ev) => {
+	console.log(ev);
 });

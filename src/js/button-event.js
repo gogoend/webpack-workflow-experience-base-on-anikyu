e0b67db7719ff489bf7712f136e49289f6ec14e3
gogoend <gogoend@qq.com>
2021-09-06 08:10:38 +0800

    feat(rxjs): 尝试创建Observable实例

--- src/js/button-event.js
+++ src/js/button-event.js
@@ -1,4 +1,4 @@
-import { fromEvent } from 'rxjs';
+import { fromEvent, Observable } from 'rxjs';
 
 const button = document.getElementById('console-btn');
 
@@ -6,3 +6,22 @@ const button = document.getElementById('console-btn');
 fromEvent(button, 'click').subscribe((ev) => {
 	console.log(ev);
 });
+
+const someObservable = new Observable(subscriber => {
+	subscriber.next(1);
+	subscriber.next(2);
+	subscriber.next(3);
+	setTimeout(() => {
+		subscriber.next(4);
+		subscriber.complete();
+		console.log(subscriber);
+	}, 1000);
+});
+
+console.log('just before subscribe');
+someObservable.subscribe({
+	next (x) { console.log('got value ' + x); },
+	error (err) { console.error('something wrong occurred: ' + err); },
+	complete () { console.log('done'); }
+});
+console.log('just after subscribe');

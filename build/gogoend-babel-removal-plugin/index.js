a881b8e7ee875267ae254765fc9a6f5f9f5733a6
gogoend <gogoend@qq.com>
2022-06-14 09:33:52 +0800

    feat(babel): 完善检查当前节点是否满足父节点为if语句，且node.name（判断条件）是否为DEBUG标识符的逻辑

--- build/gogoend-babel-removal-plugin/index.js
+++ build/gogoend-babel-removal-plugin/index.js
@@ -1,10 +1,22 @@
-module.exports = () => {
+module.exports = ({ types: t}) => {
   return {
     visitor: {
       // 标识符
       Identifier (path) {
         console.log(path.node.name)
         console.log('Identifier')
+
+        const parentIsIf = t.isIfStatement(path.parentPath)
+        const isDebug = path.node.name === 'DEBUG'
+        console.log(parentIsIf, isDebug)
+
+        // 检查当前节点是否满足父节点为if语句且node.name（判断条件）是否为DEBUG标识符
+        if (
+          parentIsIf &&
+          isDebug
+        ) {
+          // TODO:
+        }
       }
     }
   }

a641b51836c97fc68c881e0f31c15cbd78e28018
gogoend <gogoend@qq.com>
2022-06-15 10:14:45 +0800

    build(babel): 测试所编写的babel插件在Ts文件中是否生效；同时完善代码风格及类型定义

--- src/another.ts
+++ src/another.ts
@@ -3,3 +3,14 @@ import('lodash/cloneDeep').then((cloneDeep) => {
 
 	console.log(11211);
 });
+
+declare var GOGOEND: any;
+
+console.log('Ts', 'RUN!!!','GOGOEND!!!');
+/**
+ * if块最终将被我们所编写的Babel插件删除
+ */
+// @ts-ignore
+if(GOGOEND) {
+	console.log('Ts', 'STOP!!!','GOGOEND!!!');
+}

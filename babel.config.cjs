eb9d4793e1f2d6d81da2ac8e689b9df804ea1c32
gogoend <gogoend@qq.com>
2022-06-28 11:21:19 +0800

    build(babel.config.js): 更改babel.config.js配置 - 将导出内容更改为一个函数

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,30 +1,33 @@
-module.exports = {
-	'presets': [
-		[
-			'@babel/env',
-			{
-				'modules': false,
-				'useBuiltIns': 'usage',
-				'corejs':3
-			}
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		'presets': [
+			[
+				'@babel/env',
+				{
+					'modules': false,
+					'useBuiltIns': 'usage',
+					'corejs':3
+				}
+			]
+		],
+		'plugins': [
+			'@babel/plugin-external-helpers',
+			[
+				'@babel/plugin-transform-runtime',
+				{
+					'useESModules': false
+				}
+			],
+			[
+				'babel-plugin-import',
+				{
+					'libraryName': 'lodash',
+					'libraryDirectory': '',
+					'camel2DashComponentName': false  // default: true
+				}
+			],
+			'module:gogoend-babel-false-flag-condition-removal-plugin'
 		]
-	],
-	'plugins': [
-		'@babel/plugin-external-helpers',
-		[
-			'@babel/plugin-transform-runtime',
-			{
-				'useESModules': false
-			}
-		],
-		[
-			'babel-plugin-import',
-			{
-				'libraryName': 'lodash',
-				'libraryDirectory': '',
-				'camel2DashComponentName': false  // default: true
-			}
-		],
-		'module:gogoend-babel-false-flag-condition-removal-plugin'
-	]
+	};
 };

893da3235acb8ebf5adce048969b7d28e2cfaeb3
gogoend <gogoend@qq.com>
2022-06-30 03:11:42 +0800

    feat(vue-cli demo): 在纯js项目组，加入装饰器相关语法 - 以尝试探索vue-cli是如何做到能自然而言地运行带装饰器语句的代码的

--- packages/gogoend-vue-cli-demo/src/App.vue
+++ packages/gogoend-vue-cli-demo/src/App.vue
@@ -8,6 +8,27 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+function Log (Ctor, key, target) {
+  let originValue = target.value
+  target.value = function (...args) {
+    console.log(`${key}将被调用`, args)
+    originValue.call(this, ...args)
+    console.log(`${key}已被调用`, args)
+  }
+}
+
+class Phone {
+  constructor(phoneOwnerName) {
+    this.phoneOwnerName = phoneOwnerName
+  }
+  @Log
+  callTo(someone) {
+    console.warn(`${this.phoneOwnerName}正在呼叫${someone}`)
+  }
+}
+
+console.log(new Phone('gogoend').callTo('marry'))
+
 export default {
   name: 'App',
   components: {
